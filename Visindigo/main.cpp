ae87e2edd7b2ca939dc1149099eea2ecb47154de
Yayin Tsing <bingshihuakong@163.com>
2022-10-31 23:30:13 +0800

    3.11根本不好用（确信

--- Visindigo/main.cpp
+++ Visindigo/main.cpp
@@ -3,12 +3,16 @@
 #include <Python.h>
 
 int main(int* argc, char* argv[]) {
-	qDebug() << "Hello World";
 	Py_SetPythonHome(L"./Python");
 	qDebug() << "Visindigo正在初始化内置Python核心";
 	Py_Initialize();
 	qDebug() << "Visindigo的Python核心下具有以下目录:";
 	PyRun_SimpleString("import os");
 	PyRun_SimpleString("print(os.listdir())");
+	PyRun_SimpleString("print(os.getcwd())");
+	PyRun_SimpleString("import Users_Data.repos.PyVIApplication1.main as UserMain");
+	qDebug() << "定位VImain主函数";
+	PyRun_SimpleString("main = UserMain.Main()");
+	getchar();
 	return 0;
 }
---
+++ x64/Visindigo/PyVI/VICore/VICore.py
@@ -0,0 +1,5 @@
+from PyVI.VICore.VITrans import *
+class VICore:
+    @staticmethod
+    def getVisindigoVersion():
+        return inputV("VI.VICore.getVisindigoVersion")
