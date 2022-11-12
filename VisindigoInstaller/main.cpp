3210fd87677539590fea46315dbf18f8dc8487d3
Yayin Tsing <bingshihuakong@163.com>
2022-11-12 11:43:55 +0800

    建立项目VisindigoInstaller

---
+++ VisindigoInstaller/main.cpp
@@ -0,0 +1,9 @@
+﻿#include <QtCore>
+#include <QtGui>
+#include <QtWidgets>
+
+int main(int argc, char* argv[]) {
+	QApplication app(argc, argv);
+	qDebug() << "Hello world!";
+	return app.exec();
+}
